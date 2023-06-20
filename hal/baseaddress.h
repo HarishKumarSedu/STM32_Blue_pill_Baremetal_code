453b54f939264da59822572ba2ba5bd6addfc8de
Hari <harkum@inventvm.com>
2023-06-20 17:05:35 +0200

    base address header file added

---
+++ hal/baseaddress.h
@@ -0,0 +1,101 @@
+/**
+ * @file baseaddress.h
+ * @author Harish Kumar Shivaramappa (harishkumarsedu@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-06-20
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+
+#ifndef BASE_ADDRESS_H__
+#define BASE_ADDRESS_H__
+
+
+// #define CRC_BASE_ADDRESS                             0x40023000
+// #define FLASH_MEMORY_INTERFACE_BASE_ADDRESS          0x40022000
+// #define Reset_AND_CLOCK_CONTROL_RCC_BASE_ADDRESS     0x40021000
+// #define DMA1_BASE_ADDRESS                            0x40020000
+// #define TIM17_BASE_ADDRESS                           0x40014800
+// #define TIM16_BASE_ADDRESS                           0x40014400
+// #define TIM15_BASE_ADDRESS                           0x40014000
+// #define Reserved _BASE_ADDRESS                       0x40013c00
+// #define USART1 _BASE_ADDRESS                         0x40013800
+// #define Reserved _BASE_ADDRESS                       0x40013400
+// #define SPI1_BASE_ADDRESS                            0x40013000
+// #define TIM1_BASE_ADDRESS                            0x40012c00
+// #define ADC1_BASE_ADDRESS                            0x40012400
+// #define GPIO_PORT_E_BASE_ADDRESS                     0x40011800
+// #define GPIO_PORT_D_BASE_ADDRESS                     0x40011400
+// #define GPIO_PORT_C_BASE_ADDRESS                     0x40011000
+// #define GPIO_PORT_B_BASE_ADDRESS                     0x40010c00
+// #define GPIO_PORT_A_BASE_ADDRESS                     0x40010800
+// #define EXTI_BASE_ADDRESS                            0x40010400
+// #define AFIO_BASE_ADDRESS                            0x40010000
+// #define CEC_BASE_ADDRESS                             0x40007800
+// #define DAC_BASE_ADDRESS                             0x40007400
+// #define Power_CONTROL_PWR_BASE_ADDRESS               0x40007000
+// #define Backup_REGISTERS_BASE_ADDRESS                0x40006c00
+// #define I2C2 _BASE_ADDRESS                           0x40005800
+// #define I2C1_BASE_ADDRESS                            0x40005400
+// #define USART3_BASE_ADDRESS                          0x40004800
+// #define USART2_BASE_ADDRESS                          0x40004400
+// #define SPI2_BASE_ADDRESS                            0x40003800
+// #define INDEPENDENT_WATCHDOG_BASE_ADDRESS            0x40003000
+// #define WINDOW_WATCHDOG_BASE_ADDRESS                 0x40002c00
+// #define RTC_BASE_ADDRESS                             0x40002800
+// #define TIM7_BASE_ADDRESS                            0x40001400
+// #define TIM6_BASE_ADDRESS                            0x40001000
+// #define TIM4_BASE_ADDRESS                            0x40000800
+// #define TIM3_BASE_ADDRESS                            0x40000400
+// #define TIM2_BASE_ADDRESS                            0x40000000
+
+#define CRC_BASE_ADDRESS                                    0x40023000
+#define FLASH_MEMORY_INTERFACE_BASE_ADDRESS                 0x40022000
+#define RESET_AND_CLOCK_CONTROL_RCC_BASE_ADDRESS            0x40021000
+#define DMA2_BASE_ADDRESS                                   0x40020400
+#define DMA1_BASE_ADDRESS                                   0x40020000
+#define TIM17_BASE_ADDRESS                                  0x40014800
+#define TIM16_BASE_ADDRESS                                  0x40014400
+#define TIM15_BASE_ADDRESS                                  0x40014000
+#define USART1_BASE_ADDRESS                                 0x40013800
+#define SPI1_BASE_ADDRESS                                   0x40013000
+#define TIM1_BASE_ADDRESS                                   0x40012C00
+#define ADC1_BASE_ADDRESS                                   0x40012400
+#define GPIO_PORT_G_BASE_ADDRESS                            0x40012000
+#define GPIO_PORT_F_BASE_ADDRESS                            0x40011C00
+#define GPIO_PORT_E_BASE_ADDRESS                            0x40011800
+#define GPIO_PORT_D_BASE_ADDRESS                            0x40011400
+#define GPIO_PORT_C_BASE_ADDRESS                            0x40011000
+#define GPIO_PORT_B_BASE_ADDRESS                            0x40010C00
+#define GPIO_PORT_A_BASE_ADDRESS                            0x40010800
+#define EXTI_BASE_ADDRESS                                   0x40010400
+#define AFIO_BASE_ADDRESS                                   0x40010000
+#define CEC_BASE_ADDRESS                                    0x40007800
+#define DAC_BASE_ADDRESS                                    0x40007400
+#define POWER_BASE_ADDRESS                                  0x40007000
+#define BACKUP_REGISTERS_BASE_ADDRESS                       0x40006C00
+#define I2C2_BASE_ADDRESS                                   0x40005800
+#define I2C1_BASE_ADDRESS                                   0x40005400
+#define UART5_BASE_ADDRESS                                  0x40005000
+#define UART4_BASE_ADDRESS                                  0x40004C00
+#define USART3_BASE_ADDRESS                                 0x40004800
+#define USART2_BASE_ADDRESS                                 0x40004400
+#define SPI3_BASE_ADDRESS                                   0x40003C00
+#define SPI2_BASE_ADDRESS                                   0x40003800
+#define INDEPENDENT_WATCHDOG_BASE_ADDRESS                   0x40003000
+#define WINDOW_WATCHDOG_BASE_ADDRESS                        0x40002C00
+#define RTC_BASE_ADDRESS                                    0x40002800
+#define TIM14_BASE_ADDRESS                                  0x40002000
+#define TIM13_BASE_ADDRESS                                  0x40001C00
+#define TIM12_BASE_ADDRESS                                  0x40001800
+#define TIM7_BASE_ADDRESS                                   0x40001400
+#define TIM6_BASE_ADDRESS                                   0x40001000
+#define TIM5_BASE_ADDRESS                                   0x40000C00
+#define TIM4_BASE_ADDRESS                                   0x40000800
+#define TIM3_BASE_ADDRESS                                   0x40000400
+#define TIM2_BASE_ADDRESS                                   0x40000000
+
+#endif
